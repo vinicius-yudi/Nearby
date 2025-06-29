0f9f41361b32d31f7dc971e2315cb7ba25d5c4b5
Vinicius Yudi <vinizzly2005@gmail.com>
2025-06-28 22:23:52 -0300

    add -> Mocks and Details

--- app/src/main/java/com/example/nearby/ui/component/category/NearbyCategoryFilterChipList.kt
+++ app/src/main/java/com/example/nearby/ui/component/category/NearbyCategoryFilterChipList.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.nearby.data.model.Category
+import com.example.nearby.data.model.mock.mockCategories
 
 @Composable
 fun NearbyCategoryFilterChipList(
@@ -54,24 +55,7 @@ fun NearbyCategoryFilterChipList(
 private fun NearbyCategoryFilterChipListPreview() {
     NearbyCategoryFilterChipList(
         modifier = Modifier.fillMaxWidth(),
-        categories = listOf(
-            Category(
-                id = "1",
-                name = "Alimentação"
-            ),
-            Category(
-                id = "2",
-                name = "Cinema"
-            ),
-            Category(
-                id = "3",
-                name = "Farmácia"
-            ),
-            Category(
-                id = "4",
-                name = "Supermercado"
-            )
-        ),
+        categories = mockCategories,
         onSelectCategoryChanged = {}
     )
 }
